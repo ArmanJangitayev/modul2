5e2c34a27f319c839b120fa052741d47360297e7
ArmanJangitayev <148360306+ArmanJangitayev@users.noreply.github.com>
2023-12-27 09:37:57 +0600

    2.22 Создание Проекта MyBlog Создание представления для отображения(view)

---
+++ 21/MyBlog/posts/views.py
@@ -0,0 +1,48 @@
+# Posts/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_form.html', {'form': form})
+
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_form.html', {'form': form})
+
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
